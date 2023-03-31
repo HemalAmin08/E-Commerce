7ae1f4f8a1c6b6187722de2e5cc094e56a3d74ab
riddhiparmar48 <riddhi.parmar@aglowiditsolutions.com>
2023-03-31 12:34:12 +0530

    no of wishlisted item

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,20 +3,25 @@ import "./style.css";
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function Navbar() {
+export default function Navbar({ activeNumber }) {
   return (
-    <div className="navbar">
-      <Container>
-        <Link to={`/wishlist`}>
-          <Button
-            variant="outlined"
-            className="btnstyle"
-            sx={{ float: "right", marginTop: "10px" }}
-          >
-            Wishlist
-          </Button>
-        </Link>
-      </Container>
-    </div>
+    <>
+      <div className="navbar">
+        <Container
+          sx={{ display: "flex", alignItems: "center", justifyContent: "end" }}
+        >
+          <span>{activeNumber.length}</span>
+          <Link to={`/wishlist`}>
+            <Button
+              variant="outlined"
+              className="btnstyle"
+              sx={{ float: "right", marginTop: "10px" }}
+            >
+              Wishlist
+            </Button>
+          </Link>
+        </Container>
+      </div>
+    </>
   );
 }
