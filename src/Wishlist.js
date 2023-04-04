1d4a194bcfd9a67fd1176ae253f0b71bd97d91a9
riddhiparmar48 <riddhi.parmar@aglowiditsolutions.com>
2023-04-04 12:45:06 +0530

    context data used and passed

--- src/Wishlist.js
+++ src/Wishlist.js
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useContext } from "react";
+import DataContext from "./DataContext";
 
 export default function Wishlist() {
+  const activeProductData = useContext(DataContext);
+  console.log(activeProductData, "activeProductData");
   return <div>Wishlist</div>;
 }
